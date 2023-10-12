2a2682e8a4847eba9f7de98ec963d0cddb4e2cbb
Nico Goux <80793663+NicoGoux@users.noreply.github.com>
2023-10-11 21:56:20 -0300

    Resuelvo la practica 1.2

---
+++ src/main/java/isi/dan/practicas/practica1/service/MemoryDB.java
@@ -0,0 +1,68 @@
+package isi.dan.practicas.practica1.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import isi.dan.practicas.practica1.model.Alumno;
+import isi.dan.practicas.practica1.model.Curso;
+import isi.dan.practicas.practica1.model.Docente;
+
+public class MemoryDB {
+	private Integer idDocente;
+	private Integer idAlumno;
+	private Integer idCurso;
+
+	private List<Docente> listaDocentes;
+	private List<Alumno> listaAlumnos;
+	private List<Curso> listaCursos;
+	/**
+	 * @param idDocente
+	 * @param idAlumno
+	 * @param idCurso
+	 * @param listaDocentes
+	 * @param listaAlumnos
+	 * @param listaCursos
+	 */
+	public MemoryDB(Integer idInicial) {
+		this.idDocente = idInicial;
+		this.idAlumno = idInicial;
+		this.idCurso = idInicial;
+		this.listaDocentes = new ArrayList<>(0);
+		this.listaAlumnos = new ArrayList<>(0);
+		this.listaCursos = new ArrayList<>(0);
+	}
+	
+	/**
+	 * @return the listaDocentes
+	 */
+	public List<Docente> getListaDocentes() {
+		return listaDocentes;
+	}
+	/**
+	 * @return the listaAlumnos
+	 */
+	public List<Alumno> getListaAlumnos() {
+		return listaAlumnos;
+	}
+	/**
+	 * @return the listaCursos
+	 */
+	public List<Curso> getListaCursos() {
+		return listaCursos;
+	}
+
+	public Integer siguienteIdDocente() {
+		this.idDocente++;
+		return this.idDocente;
+	}
+
+	public Integer siguienteIdAlumno() {
+		this.idAlumno++;
+		return this.idAlumno;
+	}
+
+	public Integer siguienteIdCurso() {
+		this.idCurso++;
+		return this.idCurso;
+	}
+}
--- src/main/java/isi/dan/practicas/practica1/service_impl/DocenteServiceImpl.java
+++ src/main/java/isi/dan/practicas/practica1/service_impl/DocenteServiceImpl.java
@@ -1,36 +1,36 @@
 package isi.dan.practicas.practica1.service_impl;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import isi.dan.practicas.practica1.exception.RecursoNoEncontradoException;
 import isi.dan.practicas.practica1.model.Docente;
 import isi.dan.practicas.practica1.service.DocenteService;
+import isi.dan.practicas.practica1.service.MemoryDB;
 
 @Service
 public class DocenteServiceImpl implements DocenteService {
 
-	private static Integer ID = 1;
-	private static List<Docente> listaDocentes = new ArrayList<>();
+	@Autowired
+	private static MemoryDB memoryDB;
 
 	@Override
 	public Docente guardarDocente(Docente d) throws RecursoNoEncontradoException {
 		if (d.getId() == null) {
-			d.setId(ID);
-			ID++;
-			listaDocentes.add(d);
+			d.setId(memoryDB.siguienteIdDocente());
+			memoryDB.getListaDocentes().add(d);
 		}
 		else {
 			Integer idDocente = d.getId();
-			Optional<Docente> docenteEncontrado = listaDocentes.stream()
+			Optional<Docente> docenteEncontrado = memoryDB.getListaDocentes().stream()
 					.filter(docente -> docente.getId() == idDocente)
 					.findFirst();
 			if (docenteEncontrado.isPresent()) {
-				int indexOf = listaDocentes.indexOf(docenteEncontrado.get());
-				listaDocentes.add(indexOf, d);
+				int indexOf = memoryDB.getListaDocentes().indexOf(docenteEncontrado.get());
+				memoryDB.getListaDocentes().add(indexOf, d);
 			}
 			else {
 				throw new RecursoNoEncontradoException("Docente", idDocente);
@@ -42,7 +42,7 @@ public class DocenteServiceImpl implements DocenteService {
 
 	@Override
 	public Optional<Docente> buscarDocentePorId(Integer id) throws RecursoNoEncontradoException {
-		Optional<Docente> docenteEncontrado = listaDocentes.stream()
+		Optional<Docente> docenteEncontrado = memoryDB.getListaDocentes().stream()
 				.filter(docente -> docente.getId() == id)
 				.findFirst();
 		if (docenteEncontrado.isPresent()) {
@@ -55,17 +55,17 @@ public class DocenteServiceImpl implements DocenteService {
 
 	@Override
 	public List<Docente> listarDocentes() {
-		return listaDocentes;
+		return memoryDB.getListaDocentes();
 	}
 
 	@Override
 	public void bajaDocente(Integer id) throws RecursoNoEncontradoException {
-		Optional<Docente> docenteEncontrado = listaDocentes.stream()
+		Optional<Docente> docenteEncontrado = memoryDB.getListaDocentes().stream()
 				.filter(docente -> docente.getId() == id)
 				.findFirst();
 		if (docenteEncontrado.isPresent()) {
-			int indexOf = listaDocentes.indexOf(docenteEncontrado.get());
-			listaDocentes.remove(indexOf);
+			int indexOf = memoryDB.getListaDocentes().indexOf(docenteEncontrado.get());
+			memoryDB.getListaDocentes().remove(indexOf);
 		}
 		else {
 			throw new RecursoNoEncontradoException("Docente", id);
